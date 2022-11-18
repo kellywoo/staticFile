99ac9125a9dc63c7205bf25e113f23fef2a34c18
kellywoo <kelly@ent-bc.com>
2022-11-18 13:05:40 +0900

    task: remove copy after build

--- scripts/copy.js
+++ scripts/copy.js
@@ -2,9 +2,9 @@ const fs = require('fs-extra')
 const { resolveApp } = require('../config/resolveApp')
 const mainFolder = resolveApp('build/main')
 const indexFile = resolveApp('build/index.html')
-fs.copySync(resolveApp('build'), resolveApp('..'), {
-	dereference: true,
-	filter: (file) => {
-		return file !== indexFile && !file.startsWith(mainFolder)
-	},
-})
+// fs.copySync(resolveApp('build'), resolveApp('..'), {
+// 	dereference: true,
+// 	filter: (file) => {
+// 		return file !== indexFile && !file.startsWith(mainFolder)
+// 	},
+// })
